18b5188c908b79f7541433b01293471243c8513f
Alexander Miehe <alexander.miehe@flaconi.de>
2020-02-07 14:04:52 +0100

    FL-0 - add output of domain name
    * make it correct

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "name" {
-  value       = [aws_cloudfront_distribution.this.domain_name]
+  value       = aws_cloudfront_distribution.this.domain_name
   description = "The domain names of the CloudFront distributions"
 }
