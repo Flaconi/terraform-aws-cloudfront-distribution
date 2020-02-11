43caaa6b2f86433a5454e111a2d3e07490ff7e09
Alexander Miehe <alexander.miehe@flaconi.de>
2020-02-11 14:24:33 +0100

    FL-0 - add output of domain name
    * add also CloudFront id

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "name" {
   value       = aws_cloudfront_distribution.this.domain_name
-  description = "The domain names of the CloudFront distributions"
+  description = "The domain name corresponding to the distribution. For example: d604721fxaaqy9.cloudfront.net"
+}
+
+output "id" {
+  value       = aws_cloudfront_distribution.this.id
+  description = "The identifier for the distribution. For example: EDFDVBD632BHDS5."
 }
